9e10b0e340505168287848de1a57b3dfa9d3d161
gongdapeng <gongdapeng@bytedance.com>
2018-03-21 10:29:17 +0800

    feat(init): first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# sss
+
+A simple static server.
---
+++ index.js
@@ -0,0 +1,43 @@
+#!/usr/bin/env node
+
+const path = require('path');
+const http = require('http');
+const serveStatic = require('serve-static');
+const serveIndex = require('serve-index');
+// const contentDisposition = require('content-disposition'); // 如果需要支持文件下载
+
+
+// 各种路径的深坑：https://github.com/imsobear/blog/issues/48
+// console.log(__dirname);
+// console.log(__filename);
+// console.log(process.cwd());
+// console.log(path.resolve('./'));
+const serve = serveStatic(process.cwd());
+const index = serveIndex(process.cwd(), {'icons': true});
+
+const errorHandlerWrapper = (req, res) => {
+  return function() {
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end('file or directory not exist');
+  }
+}
+
+const server = http.createServer((req, res) => {
+  const errorHandler = errorHandlerWrapper(req, res);
+  serve(req, res, function(err) {
+    if (err) {
+      res.end(err);
+    } else {
+      index(req, res, errorHandler);
+    }
+  });
+});
+
+const port = 3000;
+server.listen(port);
+server.on('listening', () => {
+  console.log('listening at port ' + port);
+});
+server.on('error', (e) => {
+  console.error(e);
+});
